d65025ed0e739c593e56d326ffd37fafb7a17841
freshlhy <freshlhy@gmail.com>
2014-06-08 18:47:04 -0700

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by freshlhy on 6/8/14.
+//  Copyright (c) 2014 freshlhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
